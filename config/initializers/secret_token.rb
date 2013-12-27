b09922c920329aafc05b5438d1ee07fcb438b43e
Erica Thatcher <erica.thatcher@puppetlabs.com>
2013-12-27 09:33:02 -0800

    Angular Tutorial up to adding CSS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularCasts::Application.config.secret_key_base = '7741d277c90dd3b0909e39fe0053dbd7ea60cb59fee2045ed95d403b99c5ea928ee6fae5fff2d18f498ae98481c7a61bb653bd77eed0ee08d4e1adc09dcb0729'
